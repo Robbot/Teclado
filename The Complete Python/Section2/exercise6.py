6076b62c897bcee3e17d9edf8928e6203db69e96
Robbot <1743138+Robbot@users.noreply.github.com>
2024-09-23 22:52:18 +0100

    project refresher

--- The Complete Python/Section2/exercise6.py
+++ The Complete Python/Section2/exercise6.py
@@ -1,9 +1,9 @@
 for n in range(1,101):
     if n%3 == 0 and n%5 == 0:
-        print("fizzbuzz")
+        print("FizzBuzz")
     elif n%3 == 0:
-        print("fizz")
+        print("Fizz")
     elif n%5 == 0:
-        print("buzz")
+        print("Buzz")
     else:
         print(n)
