c837df558409a30971c8f0b76dc44b038a843979
Robbot <1743138+Robbot@users.noreply.github.com>
2024-09-26 21:32:09 +0100

    magic methods

--- The Complete Python/Section4/exercise10.py
+++ The Complete Python/Section4/exercise10.py
@@ -3,24 +3,37 @@ class Club:
     def __init__(self, name):
         self.name = name
         self.players = []
-
+    
+    # optional
+    def __len__(self):
+        return len(self.players)
+    
     # define a method that allows us to access the i-th player in the club directly via indexing.
     # for example, if some_club is an object of Club class,
     # we can access the i-th player in some_club like this (you may assume i is always valid):
     # some_club[i]
-
-
+    def __getitem__(self, i):
+        return self.players[i]
+    
     # define a method that returns a string representation of this object,
     # which can be used to recreate this object.
     # The return value should be in such format (beware of the spacing):
     # Club {club_name}: {list_of_players}
     # the club_name and list_of_players should be replaced by the according value of current object
-
-
+    def __repr__(self):
+        return f"Club {self.name}: {self.players}"
+    
     # define a method that returns a readable string representation of this object for the user.
     # The return value should be in such format (beware of the spacing):
     # Club {club_name} with {count_of_players} players
     # the club_name and count_of_players should be replaced by the according value of current object
+    def __str__(self):
+        return f"Club {self.name} with {len(self)} players"
 
 
-# You only need to finish the methods, we will take care of the object creation and call those methods for you!
+# You only need to finish the method, we will take care of the object creation and call those methods for you!
+
+myClub = Club('Arsenal')
+myClub.players.append('Rob')
+print(myClub[0])
+print(myClub)
